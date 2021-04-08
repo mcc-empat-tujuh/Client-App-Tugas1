7fdb9d43d10beba0c29740f9fdbad33c62379c43
Jaka Brajadenta <jakabrajadenta@gmail.com>
2021-04-08 10:17:51 +0700

    rebuild local project

--- src/main/java/mii/co/id/clientappmcc/ClientAppMccApplication.java
+++ src/main/java/mii/co/id/clientappmcc/ClientAppMccApplication.java
@@ -2,7 +2,7 @@ package mii.co.id.clientappmcc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//SEMANGAT REBUILD UDAH OKE
 @SpringBootApplication
 public class ClientAppMccApplication {
 
